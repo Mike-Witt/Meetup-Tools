4e3aaf67cc4e0a7e02d69c85f1aa389893414f86
Mike-Witt <msg2mw@gmail.com>
2016-06-19 13:16:36 -0600

    in progress...

---
+++ Find-Locations.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#
+# Add geographic coordinates to the location dictionary
+#
+
+import pickle
+import sys
+from geopy.geocoders import Nominatim
+from pyzipcode import ZipCodeDatabase
+
